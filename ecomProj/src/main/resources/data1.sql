04ee76f9cec7155735ab693bea2fdd1b548782ad
mallik0715 <mallik0715@gmail.com>
2024-09-25 00:22:50 +0530

    MIKED

---
+++ ecomProj/src/main/resources/data1.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name, desc, brand, price, category, release_date, available, QUNATITY) VALUES
+('Toyota Camry', 'Latest model with advanced features', 'TechBrand', 799.99, 'Electronics', '2024-08-01', TRUE, 50),
+('Honda Civic', 'Noise-canceling over-ear headphones', 'AudioMax', 199.99, 'Accessories', '2024-07-15', TRUE, 150),
+('Ford Mustang', 'High-performance laptop for professionals', 'CompuWorld', 1599.99, 'Computers', '2024-06-20', TRUE, 30),
+('Chevrolet Malibu', 'Wearable tech with fitness tracking', 'GadgetGuru', 249.99, 'Wearables', '2024-05-10', TRUE, 75),
+('BMW X5', 'Ultra HD television with smart features', 'VisionPlus', 1099.99, 'Home Entertainment', '2024-04-25', TRUE, 20);
